6bf2ab70f7b8a9bada641f81aaff4e5cb6167c16
salmasowdagarcarballo <26salma.carballo@lwhs.org>
2025-01-21 19:28:41 +0000

    submitting my first code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,31 @@
+let color = "red";
+
+let colors = ["red", "green", "yellow"];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
+  background(14, 25, 150);
+  eyes(350,350);
 }
 
 function draw() {
-  background(200);
-  
+
+}
+function eyes (x,y) {
+  //left eye
+  fill("white");
+  ellipse(x-170,x-25,220,108);
+  fill("brown");
+  ellipse(x-170,y-19,80,80);
+  fill("black");
+  ellipse(x-170,y-19,40,40);
+
+  //right eye
+  fill("white");
+  ellipse(x+175,y-25,220,108);
+  fill("brown");
+  ellipse(x+170,y-19,80,80);
+  fill("black");
+  ellipse(x+170,y-19,40,40);
 }
